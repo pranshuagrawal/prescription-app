aa8ac90c5e8bec124d6aba797e28bccc684d7461
Pranshu Agrawal <pranshuagrawal@Pranshus-MacBook-Pro-2.local>
2023-09-25 20:58:48 +0530

    Fixing UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -21,3 +21,21 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+document.addEventListener("gesturestart", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  // document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  // document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gestureend", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  // document.body.style.zoom = 0.99;
+});
